145baa2d6efaa943b30b73f7742f2188f6e110d8
Lefhoko <bobaathebelefhoko@gmail.com>
2024-07-13 11:44:03 +0200

    Initial commit

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,9 +1,6 @@
 "use client"
 import { title } from "@/components/primitives"; 
-import Form from "@/components/Form";
-import UploadForm from "@/components/UploadForm"; 
-import AllFilesPage from "@/components/allfiles"
-import { siteConfig } from "@/config/site";
+import Form from "@/components/Form";   
 
 export default function DocsPage() {
   return (
